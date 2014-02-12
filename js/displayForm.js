1a79189d9220941ce04247c3ce9c6b0f14a6ea62
bmercier1 <bmercier1@systra.com>
2014-02-12 12:27:12 +0100

    Add Sum Up window

--- js/displayForm.js
+++ js/displayForm.js
@@ -1,5 +1,17 @@
+(function(){
+	window.addEventListener('load', onLoadHandler, false);
+})();
+
+function onLoadHandler(e)
+{
+	var page = document.getElementsByClassName('page')[0];
+	//alert(getComputedStyle(page, null).marginRight);
+	$('.sumUp').width(getComputedStyle(page, null).marginRight);
+}
+
 function displayUserForm()
 {
+
 	//lancer le script PHP pour avoir la liste des users
 	$('#selectPeople').show();
 	$select = $('#myList');
@@ -17,7 +29,6 @@ function displayUserForm()
 	})
 }
 
-
 function populateUserList(data, textStatus, jqXHR)
 {
 	//alert("proj");
@@ -27,7 +38,7 @@ function populateUserList(data, textStatus, jqXHR)
 	   $select.html('');
 	    //iterate over the data and append a select option
 	    $.each(data, function(key, val){
-	      $select.append('<li><a href="#" onClick="displayProj(this.id)" id="' + val.accountsID + '">' + val.nom_prenom + '</a></li>');
+	      $select.append('<li><a onClick="displayProj(this.id,  this.innerText)" id="' + val.accountsID + '">' + val.nom_prenom + '</a></li>');
 	    })
 	    //localStorage.setitem('mail',val.mail);
 
@@ -47,7 +58,7 @@ function createUser()
 	$('#selectPeople').hide();
 }
 
-function displayProj(id)
+function displayProj(id, value)
 {
     localStorage.setItem('accountsID',id);
 
@@ -55,6 +66,8 @@ function displayProj(id)
 	$('#progressBar').attr("aria-valuenow","15");
 
 	$('#step2').removeClass("hide");
+
+	$('.sumUp').append('<span>' + value + '<span>');
 
 }
 
@@ -85,7 +98,7 @@ function populateProjList(data, textStatus, jqXHR)
 	   $select.html('');
 	    //iterate over the data and append a select option
 	    $.each(data, function(key, val){
-	      $select.append('<li><a href="#" onClick="saveInProjAcc(this.id)" id="' + val.ProjectID + '">' + val.ProjectName + '</a></li>');
+	      $select.append('<li><a onClick="saveInProjAcc(this.id)" id="' + val.ProjectID + '">' + val.ProjectName + '</a></li>');
 	    })
 	    //localStorage.setitem('mail',val.mail);
 
@@ -109,8 +122,8 @@ function saveInProjAcc(id)
 		{
 			$('#progressBar').width("30%");		
 			$('#progressBar').attr("aria-valuenow","30");
-			var alert = document.createElement("div");
-			alert.className = "alert alert-dismissable alert-success fade";
+			var myAlert = document.createElement("div");
+			myAlert.className = "alert alert-dismissable alert-success fade";
 			var myBut = document.createElement("button");
 			myBut.setAttribute('type','button');
 			myBut.className = "close";
@@ -118,13 +131,13 @@ function saveInProjAcc(id)
 			myBut.setAttribute('aria-hidden',true);
 			myBut.innerHTML = "&times;";
 
-			alert.appendChild(myBut);
-			alert.innerHTML += "<strong>Success</strong> Project add to user";
+			myAlert.appendChild(myBut);
+			myAlert.innerHTML += "<strong>Success</strong> Project add to user";
 			var pgBar = document.getElementById('prog');
 			var parent = pgBar.parentNode;
-			parent.insertBefore(alert,pgBar);
+			parent.appendChild(myAlert);
 
-			alert.className += " in";
+			myAlert.className += " in";
 		},
 		error: function(xhr, ajaxOptions, thrownError)
 		{   //if there is an error append a 'none available' option
@@ -140,13 +153,19 @@ function saveInProjAcc(id)
 			myAlert.appendChild(myBut);
 			myAlert.innerHTML += "<strong>Error!</strong>  " + _.string.words((xhr.responseText).toString(),"}")[1];
 
-			alert(_.string.words((xhr.responseText).toString(),"}")[0]);
+			//alert(_.string.words((xhr.responseText).toString(),"}")[0]);
 			
 			var pgBar = document.getElementById('prog');
 			var parent = pgBar.parentNode;
-			parent.insertBefore(myAlert,pgBar);
+			parent.appendChild(myAlert);
+
 
 			myAlert.className += " in";
+
+			setTimeout(function(){myAlert.className.replace(/\bin\b/,''); myAlert.className+='out'}, 3000);
+
+			$('#step3').removeClass("hide");
+
 		}
 	})
 
